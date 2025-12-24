abee1efc40e7fcb90ab0aaa3dc69e6728c618cc2
Manju Verma <manjuverma2906@gmail.com>
2025-12-24 21:03:07 +0530

    feat: render Menu component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Home from "./Components/Home"
+import Menu from "./Components/Menu"
 import Navbar from "./Components/Navbar"
 
 function App() {
   return (
     <>
+    <div className='bg-amber-100'>
       <Navbar/>
       <Home/>
+      <Menu/>
+    </div>
+      
     </>
   )
 }
