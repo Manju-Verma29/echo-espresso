97c4fcd11f99783425d9c797d48160ff7bb94545
Manju Verma <manjuverma2906@gmail.com>
2025-12-28 17:34:03 +0530

    feat: update category label

--- src/Components/Menu/MenuCategory.jsx
+++ src/Components/Menu/MenuCategory.jsx
@@ -4,8 +4,8 @@ export default function MenuCategory({label, types}) {
 return (
 <>
 <section>
-<h4 className="text-amber-900 font-bold md:w-50 lg:w-70 m-10 lg:mt-10 lg:ml-20 flex flex-col items-center justify-center text-3xl">{label}</h4>
-<div className="grid grid-cols-3 border-b m-10 border-amber-900">
+<h4 className="text-amber-900 underline underline-offset-8 decoration-2 font-bold md:w-50 lg:w-70 m-10 lg:mt-10 lg:ml-20 flex flex-col items-center justify-center text-3xl">{label}</h4>
+<div className="grid grid-cols-3 border-b m-10 pb-8 border-amber-900">
   {Object.values(types).map((type, index) => (
     <MenuSubCategory 
       key={index} 
