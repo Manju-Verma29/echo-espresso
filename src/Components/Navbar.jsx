4a4d679545da5691b70b61f6f62ecd0377100937
Manju Verma <manjuverma2906@gmail.com>
2025-12-28 17:05:30 +0530

    Update font color

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,30 @@
 import logo from "../assets/logo.png";
 
-export default function Navbar({activePage, setActivePage}) {
-  return (
-    <>
-    <header className='bg-amber-100 shadow'>
-        <div className='mx-auto w-full px-6 lg:px-30 '>
-        <div className='flex xs:h-20 sm:h-20 md:h-20 lg:h-25 items-center justify-between gap-9 '>
-            <div className='flex items-center justify-between gap-3 lg:gap-5'>
-                <img src={logo} alt="logo" className='w-10 lg:w-16 ' />
-                <h5 className='text-green font-mono lg:text-3xl tracking-wider font-semibold '>
-                    ECHO ESPRESSO
-                </h5>
-            </div>
-        
+export default function Navbar() {
+return (
+<>
+<header className='bg-amber-100 shadow'>
+<div className='mx-auto w-full px-6 lg:px-30 '>
+<div className='flex xs:h-20 sm:h-20 md:h-20 lg:h-25 items-center justify-between gap-9 '>
+<div className='flex items-center justify-between gap-3 lg:gap-5'>
+    <img src={logo} alt="logo" className='w-10 lg:w-16 ' />
+    <h5 className='text-amber-900 font-mono lg:text-3xl tracking-wider font-semibold '>
+        ECHO ESPRESSO
+    </h5>
+</div>
 
-        <nav aria-label='Primary navigation' className='absolute left-1/2 ' >
-            <ul className='flex gap-8 lg:gap-25 text-sm font-medium text-green-900'> 
-                <li><a href="/" className='text-green lg:text-lg transition'>Home</a></li>
-                <li><a href="/about" className='text-green lg:text-lg transition' >About</a></li>
-                <li><a href="/menu" className='text-green lg:text-lg transition' >Menu</a></li>
-                <li><a href="/visit" className='text-green lg:text-lg transition' >Visit Us</a></li>
-                <li><a href="/socials" className='text-green lg:text-lg transition' >Socials</a></li>
-            </ul>
-        </nav>
-
-        </div>
-        </div>
-    </header>
-
-    </>
+<nav aria-label='Primary navigation' className='absolute left-1/2 ' >
+<ul className='flex gap-8 lg:gap-25 text-sm font-medium'> 
+    <li><a href="/" className='text-amber-900 lg:text-lg transition'>Home</a></li>
+    <li><a href="/about" className='text-amber-900 lg:text-lg transition' >About</a></li>
+    <li><a href="/menu" className='text-amber-900 lg:text-lg transition' >Menu</a></li>
+    <li><a href="/visit" className='text-amber-900 lg:text-lg transition' >Visit Us</a></li>
+    <li><a href="/socials" className='text-amber-900 lg:text-lg transition' >Socials</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</header>
+</>
   )
 }
