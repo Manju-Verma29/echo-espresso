ee27f2635d3eee79f4ff1bb4f06c76bcd7911bfb
Manju Verma <manjuverma2906@gmail.com>
2025-12-28 17:32:50 +0530

    feat: update image width

--- src/Components/Menu/MenuSubCategory.jsx
+++ src/Components/Menu/MenuSubCategory.jsx
@@ -8,7 +8,7 @@ return (
 <section className="grid place-items-center p-5 text-center">
 
   <div onClick={()=> setIsOpen(!isOpen)} className=" " >
-    <img className="w-48 h-48 lg:w-80 lg:h-80 object-cover mb-8 rounded-full" src={image} alt={label} />
+    <img className="w-48 h-48 lg:w-60 lg:h-60 object-cover mb-8 rounded-full" src={image} alt={label} />
     <h5 className="text-3xl text-amber-900">{label}</h5>
   </div>
 
