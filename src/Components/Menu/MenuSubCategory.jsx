0db5eda2192f47029ccd45eab5a1f07966dfca50
Manju Verma <manjuverma2906@gmail.com>
2025-12-28 16:11:44 +0530

    feat: implement expandable subcategory with item toogle

---
+++ src/Components/Menu/MenuSubCategory.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import MenuItemCard from "./MenuItemCard";
+
+export default function MenuSubCategory({label,image,items}) {
+  const [isOpen, setIsOpen] = useState(false);
+return (
+<>
+<section className="grid place-items-center p-5 text-center">
+
+  <div onClick={()=> setIsOpen(!isOpen)} className=" " >
+    <img className="w-48 h-48 lg:w-80 lg:h-80 object-cover mb-8 rounded-full" src={image} alt={label} />
+    <h5 className="text-3xl text-amber-900">{label}</h5>
+  </div>
+
+  {isOpen && (
+    <div className="bg-amber-50">
+      {items.map((item,index) => (
+          <MenuItemCard key={index} {...item}/>
+      ))}
+  </div>
+  )}
+</section>
+</>
+);
+}
