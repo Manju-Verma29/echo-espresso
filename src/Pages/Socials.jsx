a86aaa04f4b1b36aaba10cd2997a4e4bab6540ca
Manju Verma <manjuverma2906@gmail.com>
2025-12-31 21:37:52 +0530

    feat: Add Social page

---
+++ src/Pages/Socials.jsx
@@ -0,0 +1,44 @@
+
+export default function Socials() {
+  return (
+    <>
+    <main className='px-10'>
+        <article className='p-8'>
+            <h2 className='text-amber-900 text-3xl mb-2'>Connect With Us</h2>
+            <p className='text-amber-900 text-xl p-3 tracking-wide'>Follow along for quiet moments, seasonal offerings and behind-the-scenes craft.</p>
+        </article>
+
+        <article className='p-8'>
+            <h2 className='text-amber-900 text-3xl mb-6'>Social Channels</h2>
+            <div>
+            <strong className='text-lg text-amber-800'>Instagram</strong>
+            <p className='text-lg'>Visual stories of our coffee, space, and daily rituals.</p>
+            </div>
+
+            <div>
+           <strong className='text-lg text-amber-800'>Facebook</strong> 
+           <p className='text-lg'>Updates, events, and announcements.</p>
+            </div>
+
+            <div>
+           <strong className='text-lg text-amber-800'>X (Twitter)</strong> 
+            <p className='text-lg'>Short reflections, news, and limited releases.</p>
+            </div>
+        </article>
+
+        <article className='p-8'>
+            <h2 className='text-amber-900 text-3xl mb-6'>Tag Your Moments</h2>
+            <p className='text-amber-900 text-xl p-3 tracking-wide'>
+            We love seeing our space through your eyes.
+            </p>
+            <p className='text-amber-900 text-xl px-3 tracking-wide'>Tag us to be featured.</p>
+        </article>
+
+        <article className='p-8'>
+            <h2 className='text-amber-900 text-3xl mb-6'>Stay Connected</h2>
+            <p className='text-amber-900 text-xl p-3 tracking-wide'>A glimpse into our world — thoughtfully shared.</p>
+        </article>
+          </main>
+    </>
+  )
+}
