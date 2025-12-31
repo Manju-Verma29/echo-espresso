05dc2fde0b167fa0c2ec49f05af8f7dbe525e932
Manju Verma <manjuverma2906@gmail.com>
2025-12-31 21:40:18 +0530

    feat: Add gallery image file

---
+++ src/assets/gallery/galleryData.js
@@ -0,0 +1,44 @@
+import cafe1 from "./cafe1.jpg";
+import cafe2 from "./cafe2.jpg";
+import cafe3 from "./cafe3.jpg";
+import cafe4 from "./cafe4.jpg";
+import cafe5 from "./cafe5.jpg";
+import cafe6 from "./cafe6.jpg";
+import cafe7 from "./cafe7.jpg";
+import cafe8 from "./cafe8.jpg";
+import cafe9 from "./cafe9.jpg";
+import cafe10 from "./cafe10.jpg";
+import cafe11 from "./cafe11.jpg";
+import cafe12 from "./cafe12.jpg";
+import cafe13 from "./cafe13.jpg";
+import cafe14 from "./cafe14.jpg";
+import cafe15 from "./cafe15.jpg";
+import cafe16 from "./cafe16.jpg";
+import cafe17 from "./cafe17.jpg";
+import cafe18 from "./cafe18.jpg";
+import cafe19 from "./cafe19.jpg";
+import cafe20 from "./cafe20.jpg";
+
+export const galleryData = [
+    {id: 1, src: cafe1},
+    {id: 6, src: cafe6},
+    {id: 3, src: cafe3},
+    {id: 5, src: cafe5},
+    {id: 7, src: cafe7},
+    {id: 9, src: cafe9},
+    {id: 2, src: cafe2},
+    {id: 8, src: cafe8},
+    {id: 4, src: cafe4},
+    {id: 10, src: cafe10},
+    {id: 11, src: cafe11},
+    {id: 12, src: cafe12},
+    {id: 13, src: cafe13},
+    {id: 14, src: cafe14},
+    {id: 15, src: cafe15},
+    {id: 16, src: cafe16},
+    {id: 17, src: cafe17},
+    {id: 18, src: cafe18},
+    {id: 19, src: cafe19},
+    {id: 20, src: cafe20},
+];
+
