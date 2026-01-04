1c0ee6a7fecf7f6b80378f077d9be29b9c2932f3
Manju Verma <manjuverma2906@gmail.com>
2026-01-04 21:16:30 +0530

    feat: add visit section with data-driven rendering

--- src/Components/About/AboutSection.jsx
+++ src/Components/About/AboutSection.jsx
@@ -1,7 +1,7 @@
 export default function AboutSection({title,image,children}) {
   return (
     <>
-    <section className="block lg:hidden">
+   <section className="block lg:hidden">
         <h2 className="md:text-3xl lg:text-5xl pl-18 text-amber-900 py-8">{title}</h2>
         <div className="pb-10 p-5 px-10">
             <div className="m-5">
@@ -11,7 +11,7 @@ export default function AboutSection({title,image,children}) {
         </div>      
     </section>
 
-    <section className="hidden md:block" >
+    <section className="hidden lg:block" >
         <h2 className="md:text-3xl lg:text-5xl pl-18 text-amber-900 py-8">{title}</h2>
         <div className="pb-10 grid place-items-center grid-cols-2 p-5 px-10">
             <div className="grid grid-cols-2 mr-20 place-items-center">
@@ -19,7 +19,7 @@ export default function AboutSection({title,image,children}) {
             </div>
             <img src={image} alt="" />
         </div>      
-    </section>
+    </section> 
      </>
   )
 }
