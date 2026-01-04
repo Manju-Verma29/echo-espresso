a8a94e389d91781b4e1b7e25a830fbe4a6bfe27a
Manju Verma <manjuverma2906@gmail.com>
2026-01-04 18:07:57 +0530

    style(ui): update responsive layout for mobile and desktop

--- src/Components/About/AboutSection.jsx
+++ src/Components/About/AboutSection.jsx
@@ -1,13 +1,25 @@
 export default function AboutSection({title,image,children}) {
   return (
-    <section>
+    <>
+    <section className="block lg:hidden">
         <h2 className="md:text-3xl lg:text-5xl pl-18 text-amber-900 py-8">{title}</h2>
-        <div className="pb-10 grid place-items-center grid-cols-2 m-5 px-10">
-            <img src={image} alt="" />
-            <div className="place-items-center">
+        <div className="pb-10 p-5 px-10">
+            <div className="m-5">
                 {children}
             </div>
-        </div>     
+            <img src={image} alt="" />
+        </div>      
     </section>
+
+    <section className="hidden md:block" >
+        <h2 className="md:text-3xl lg:text-5xl pl-18 text-amber-900 py-8">{title}</h2>
+        <div className="pb-10 grid place-items-center grid-cols-2 p-5 px-10">
+            <div className="grid grid-cols-2 mr-20 place-items-center">
+                {children}
+            </div>
+            <img src={image} alt="" />
+        </div>      
+    </section>
+     </>
   )
 }
