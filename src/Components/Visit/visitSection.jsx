dd611667917c842939960da21a500f67436b7ffe
Manju Verma <manjuverma2906@gmail.com>
2026-01-04 22:13:48 +0530

    feat: add visit section with data-driven rendering

--- src/Components/Visit/visitSection.jsx
+++ src/Components/Visit/visitSection.jsx
@@ -3,12 +3,11 @@ export default function VisitSection({title, image, children}) {
     <>
     <section>
         <h1 className=" md:text-3xl lg:text-5xl pl-18 text-amber-900 py-8">{title}</h1>
-        <div className="pb-10 grid place-items-center grid-cols-2 m-5 px-10">
+        <div className="pb-10 grid place-items-center grid-cols-2 p-5 px-10">
             <img src={image} />
             <div className="place-items-center">
               {children}
-            </div>
-            
+            </div>     
         </div>
     </section>
     </>
