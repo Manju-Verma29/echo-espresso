eefd9fb026b8c0b18780c7a92294acef4eca1a5c
Manju Verma <manjuverma2906@gmail.com>
2026-01-05 21:29:44 +0530

    refine About section responsive layout and spacing

--- src/Components/About/AboutSection.jsx
+++ src/Components/About/AboutSection.jsx
@@ -1,23 +1,25 @@
 export default function AboutSection({title,image,children}) {
   return (
     <>
-   <section className="block lg:hidden">
-    <h2 className="md:text-3xl lg:text-5xl pl-18 text-amber-900 py-8">{title}</h2>
-    <div className="pb-10 p-5 px-10">
-        <div className="m-5">
+   <section className="block lg:hidden pt-20">
+    <h2 className="md:text-3xl lg:text-5xl pl-18 text-center text-amber-900 ">{title}</h2>
+    <div className="p-3 px-10">
+        <div className="p-2">
             {children}
         </div>
         <img src={image} alt="" />
     </div>      
     </section>
 
-    <section className="hidden lg:block" >
-    <h2 className="md:text-3xl lg:text-5xl pl-18 text-amber-900 py-8">{title}</h2>
-    <div className="pb-10 grid place-items-center grid-cols-2 p-5 px-10">
-        <div className="grid grid-cols-2 mr-20 place-items-center">
+    <section className="hidden lg:block pt-28" >
+    <div className=" grid place-items-center grid-cols-2">
+    <div>
+        <h2 className="md:text-3xl m-11 lg:text-5xl pl-18 text-amber-900">{title}</h2>
+        <div className="grid grid-cols-2 mr-10 place-items-center">
             {children}
         </div>
-        <img src={image} alt="" />
+    </div>
+        <img src={image} className="w-175" />
     </div>      
     </section> 
      </>
