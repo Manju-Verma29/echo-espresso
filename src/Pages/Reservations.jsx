999e060bca2200ed40f4749cad2a7f0d303284fc
Manju Verma <manjuverma2906@gmail.com>
2026-01-10 23:39:56 +0530

    ui: update button hover color

--- src/Pages/Reservations.jsx
+++ src/Pages/Reservations.jsx
@@ -16,7 +16,7 @@ export default function Reservations() {
       <input type="date" placeholder="Date" min={today} className="w-full border border-amber-900 p-3 text-base sm:text-xl text-amber-900 rounded-md focus:outline-none"  required/>
       <input type="number" placeholder="Number of guests" className="w-full border border-amber-900 p-3 text-base sm:text-xl text-amber-900 rounded-md focus:outline-none"  required/>
       <textarea rows={5} placeholder="Special requests" className="w-full border border-amber-900 p-3 text-base sm:text-xl text-amber-900 rounded-md focus:outline-none"  required/>
-      <input type="submit" value="Submit" className="w-full bg-amber-900 text-amber-100 p-3 text-base sm:text-xl font-bold rounded-md cursor-pointer"/>
+      <input type="submit" value="Submit" className="w-full bg-amber-900 hover:bg-amber-950 text-amber-100 p-3 text-base sm:text-xl font-bold rounded-md cursor-pointer"/>
       </div>
     </form>
   </div>
