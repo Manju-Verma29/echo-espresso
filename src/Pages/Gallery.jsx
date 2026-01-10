ff8afc163b7d82ebe33acce7fc991078e33618d1
Manju Verma <manjuverma2906@gmail.com>
2026-01-10 23:32:17 +0530

    ui: make ui responsive

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -3,8 +3,8 @@ import { galleryData } from '../assets/gallery/galleryData';
 export default function Gallery() {
   return (
     <>
-        <article className='bg-amber-100 pl-2 py-8'>
-            <div className='grid  grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 pr-2 md:gap-3 lg:gap-4 bg-amber-100'>
+        <article className='pl-2 py-8'>
+            <div className='grid  grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 pr-2 md:gap-3 lg:gap-4'>
                 {galleryData.map((img) => (
                     <div key={img.id}>
                     <img src={img.src}/>
