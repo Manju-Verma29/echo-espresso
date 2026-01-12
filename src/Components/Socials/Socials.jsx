9974fa9b4ac6dcf9820875a67532dfe289c15170
Manju Verma <manjuverma2906@gmail.com>
2026-01-12 14:46:15 +0530

    Fix background color

--- src/Components/Socials/Socials.jsx
+++ src/Components/Socials/Socials.jsx
@@ -4,7 +4,7 @@ import SocialSection from './SocialSection'
 export default function Socials() {
   return (
     <>
-    <main className='bg-amber-50'>
+    <main>
     {socialsData.map((section)=>(
         <SocialSection key={section.id} title={section.title} image={section.image} detail={section.detail}>
             {section.content}
